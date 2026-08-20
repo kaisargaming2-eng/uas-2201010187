483448e760600d118783c879003170e93d44e217
kaisargaming2-eng <kaisargaming2@gmail.com>
2026-08-20 16:12:57 +0800

    Add files via upload

---
+++ application/config/config.php
@@ -0,0 +1,3 @@
+<?php
+define('BASE_URL','http://localhost/uas_muhammad_kaisar_rahman/');
+$config=['project_name'=>'UAS Web Design','student_name'=>'Muhammad Kaisar Rahman','student_nim'=>'2201010187'];
---
+++ application/models/Student_model.php
@@ -0,0 +1,2 @@
+<?php
+class Student_model { public function profile(){ return ['name'=>'Muhammad Kaisar Rahman','nim'=>'2201010187']; } }
